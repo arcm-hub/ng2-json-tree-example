cca0d29202b1e9a102b8445a7d6159f0bf6288f1
Ashok Raja CM <achoppam@cisco.com>
2020-09-18 16:15:36 +0530

    Example 5 added to demonstrate textbox level toolbar and node deletion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { Example1Component } from './example1/example1.component';
 import { Example2Component } from './example2/example2.component';
 import { Example3Component } from './example3/example3.component';
 import { Example4Component } from './example4/example4.component';
+import { Example5Component } from './example5/example5.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { Example4Component } from './example4/example4.component';
     Example1Component,
     Example2Component,
     Example3Component,
-    Example4Component
+    Example4Component,
+    Example5Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/example5/example5.component.ts
@@ -0,0 +1,117 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example5',
+  templateUrl: './example5.component.html',
+  styleUrls: ['./example5.component.css']
+})
+export class Example5Component implements OnInit {
+  config = {
+    pageSize: 2
+  };
+  json = {
+    "breakfast_menu": {
+        "food": [
+            {
+                "calories": {
+                    "_text": "650"
+                },
+                "description": {
+                    "_text": "Two of our famous Belgian Waffles with plenty of real maple syrup"
+                },
+                "name": {
+                    "_text": "Belgian Waffles"
+                },
+                "price": {
+                    "_text": "$5.95"
+                }
+            },
+            {
+                "calories": {
+                    "_text": "900"
+                },
+                "description": {
+                    "_text": "Light Belgian waffles covered with strawberries and whipped cream"
+                },
+                "name": {
+                    "_text": "Strawberry Belgian Waffles"
+                },
+                "price": {
+                    "_text": "$7.95"
+                }
+            },
+            {
+                "calories": {
+                    "_text": "900"
+                },
+                "description": {
+                    "_text": "Light Belgian waffles covered with an assortment of fresh berries and whipped cream"
+                },
+                "name": {
+                    "_text": "Berry-Berry Belgian Waffles"
+                },
+                "price": {
+                    "_text": "$8.95"
+                }
+            },
+            {
+                "calories": {
+                    "_text": "600"
+                },
+                "description": {
+                    "_text": "Thick slices made from our homemade sourdough bread"
+                },
+                "name": {
+                    "_text": "French Toast"
+                },
+                "price": {
+                    "_text": "$4.50"
+                }
+            },
+            {
+                "calories": {
+                    "_text": "950"
+                },
+                "description": {
+                    "_text": "Two eggs, bacon or sausage, toast, and our ever-popular hash browns"
+                },
+                "name": {
+                    "_text": "Homestyle Breakfast"
+                },
+                "price": {
+                    "_text": "$6.95"
+                }
+            }
+        ]
+    }
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onEvent(event: any) {
+    var backgroundColor = 'white'
+    if(event.type === 'TREE_NODE_CREATED' && event.data.modelKey === 'food') {
+      event.data.toolbar.append('span')
+      .attr('class', 'ng2-json-tree-toolbar-button')
+      .text('highlight')
+      .on('click', function(d, i) {
+        backgroundColor = backgroundColor === 'white' ? 'lightyellow' : 'white';
+        event.data.d3ParentContainer.style('background-color', backgroundColor);
+        console.log("CLICKED", event);
+      });
+    } else if(event.type === "TEXT_INPUT_CREATED" && event.data.modelKey === 'price') {
+        event.data.toolbar.append('span')
+        .attr('class', 'ng2-json-tree-toolbar-button')
+        .text('remove')
+        .on('click', function(d, i) {
+            event.data.d3Container.remove(); //Removing UI Element
+            delete event.data.parentModel[event.data.modelKey]; // Deleting the actual data on the model
+            console.log("CLICKED", event);
+        });
+    }
+  }
+
+}
